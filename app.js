e1245aea71d9721a2554e33577fd16a2739e52ce
Zack Jackson <z.jackson58@gmail.com>
2018-09-03 12:53:00 -0400

    New game button

--- app.js
+++ app.js
@@ -13,18 +13,40 @@
  */
 
 var scores, roundScore, activePlayer;
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
 
-// Hides the dice img in the beginning
-document.querySelector(".dice").style.display = "none";
+// Initializes a new game
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
 
-// Sets all scores to 0 in the beginning
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
+  // Hides the dice img in the beginning
+  document.querySelector(".dice").style.display = "none";
+
+  // Sets all scores to 0 in the beginning
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  // Changes player names back on new game reset
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+
+  // Remove winner class
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+
+  // Remove active class
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+
+  // Make player 1 active player
+  document.querySelector(".player-0-panel").classList.add("active");
+}
+
+// Initializes a new game in the beginning
+init();
 
 // Toggles to next player and resets current score to 0
 function nextPlayer() {
@@ -79,8 +101,17 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
   if (scores[activePlayer] >= 100) {
     document.querySelector("#name-" + activePlayer).textContent = "Winner!";
     document.querySelector(".dice").style.display = "none";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
   } else {
     // Next player's turn
     nextPlayer();
   }
 });
+
+// New game initialization button
+document.querySelector(".btn-new").addEventListener("click", init);
